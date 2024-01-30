01b2dfc5a71356f86fb72e551c3ba1046d9b72fd
Gabriel.Falcao <gabrielfalchonorato18@gmail.com>
2024-01-29 22:10:42 -0300

    FindById users and Exceptions implementations

--- src/main/java/com/gabriel/springbootmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/springbootmongo/resources/UserResource.java
@@ -1,12 +1,12 @@
 package com.gabriel.springbootmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ import com.gabriel.springbootmongo.services.UserService;
 
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 	
 	@Autowired
@@ -27,5 +27,12 @@ public class UserResource {
 		List<User> list = userService.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = userService.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok().body(userDTO);
 	}
 }
--- src/main/java/com/gabriel/springbootmongo/services/UserService.java
+++ src/main/java/com/gabriel/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabriel.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.springbootmongo.domain.User;
 import com.gabriel.springbootmongo.repository.UserRepository;
+import com.gabriel.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 		List<User> list = userRepository.findAll();
 		return list; 
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+		}
 }
